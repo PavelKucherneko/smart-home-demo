dba290ea721d9e53829c86a33fc438a1780c4e43
Pavel <pavelpost1083@gmail.com>
2017-01-15 22:12:40 +0300

    Tests (unit and integrational) are added

---
+++ SmartHomeDemo.Tests/IntegrationTests/PageObjects/DevicePageObject.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace SmartHomeDemo.Tests.IntegrationTests.PageObjects
+{
+    class DevicePageObject
+    {
+        private readonly IWebDriver _driver;
+
+        public DevicePageObject(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "#device-table")]
+        private IWebElement _devicesTable;
+
+        public int GetDevicesCount()
+        {
+            return _devicesTable.FindElements(By.TagName("tr")).Count - 1; // минус строка заголовка
+        }
+    }
+}
---
+++ SmartHomeDemo.Tests/UnitTests/Controllers/DeviceInfoControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using NUnit.Framework;
+using SmartHomeDemo.Controllers;
+using SmartHomeDemo.Models;
+
+namespace SmartHomeDemo.Tests.UnitTests.Controllers
+{
+    class DeviceInfoControllerTest
+    {
+        [TestCase(3)]
+        public void GetAllDevices_CountOfDevicesIsCorrect(int aMinDeviceCount)
+        {
+            // Arrange
+            DeviceInfoController controller = new DeviceInfoController();
+
+            // Act
+            ICollection<DeviceViewModel> devices = controller.GetAllDevices();
+
+            // Assert
+            Assert.That(devices.Count >= aMinDeviceCount);
+        }
+
+        [Test]
+        public void GetDevice_CorrectIndex()
+        {
+            // Arrange
+            int id = 1;
+            DeviceInfoController controller = new DeviceInfoController();
+
+            // Act
+            OkNegotiatedContentResult<DeviceViewModel> result = controller.GetDevice(id) as OkNegotiatedContentResult<DeviceViewModel>;
+            
+            // Assert
+            Assert.NotNull(result);
+            Assert.That(result.Content.Id == id);
+        }
+
+        [Test]
+        public void GetDevice_IncorrectIndex()
+        {
+            // Arrange
+            int id = -1;
+            DeviceInfoController controller = new DeviceInfoController();
+
+            // Act
+            NotFoundResult result = controller.GetDevice(id) as NotFoundResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
